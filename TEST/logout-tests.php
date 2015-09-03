bb6d337a0ad042dfc55e211035ce11eb74faf6fc
Daniel Ruf <DanielRuf@users.noreply.github.com>
2015-09-03 15:45:51 +0200

    added working logout example

--- TEST/logout-tests.php
+++ TEST/logout-tests.php
@@ -5,7 +5,15 @@ function logout() {
     echo "You need to enter a valid username and password.";
     exit;
 }
-logout();
+logout2();
+
+function logout2() {
+	$path=str_replace("logout.php","",$_SERVER['HTTP_REFERER']);
+	echo '<script>var request = new XMLHttpRequest();                                        
+    request.open("get", "welcome", false, "false", "false");                                                                                                                               
+    request.send();
+	window.location.replace("'.$path.'");</script>';
+}
 
 echo "Welcome <a href=\"javascript:void(0)\" onclick=\"logout()\">logout</a>";
 echo <<<CODE
